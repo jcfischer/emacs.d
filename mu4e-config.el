5262cf5dd660777df1f3087f2e66f206aa7d686f
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2016-09-21 09:18:28 +0200

    More mu4e configurations
    * Remove account switiching in favor of contexts
    * Global keybinding
    * set some fancy stuff

--- mu4e-config.el
+++ mu4e-config.el
@@ -13,6 +13,9 @@
 
 (require 'org-mu4e)
 
+
+(global-set-key (kbd "C-c u") 'mu4e)
+
 ;; Default account on startup
 (setq user-full-name  "Jens-Christian Fischer"
       mu4e-sent-folder "/invisible/INBOX.Sent"
@@ -28,8 +31,12 @@
 
 ;; HTML Mails
 (require 'mu4e-contrib)
-(setq mu4e-html2text-command 'mu4e-shr2text)
+;; (setq mu4e-html2text-command 'mu4e-shr2text)
 (add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+
+;; html handling
+;; (setq mu4e-html2text-command
+;;   "textutil -stdin -format html -convert txt -stdout")
 
 ;; Alternatives are the following, however in first tests they
 ;; show inferior results
@@ -37,73 +44,14 @@
 ;; (setq mu4e-html2text-command "html2text -utf8 -width 72")
 ;; (setq mu4e-html2text-command "w3m -dump -T text/html")
 
+;; my home server uses a self signed certificate. Pacify SMTP
 (setq starttls-use-gnutls t)
 (setq starttls-gnutls-program "gnutls-cli")
- (setq starttls-extra-arguments '("--insecure"))
+(setq starttls-extra-arguments '("--insecure"))
 
 
-(defvar my-mu4e-account-alist
-  '(("invisible"
-     (mu4e-sent-folder "/invisible/INBOX.Sent")
-     (mu4e-drafts-folder "/invisible/INBOX.Drafts")
-     (mu4e-trash-folder "/invisible/INBOX.Trash")
-     (user-mail-address "jens-christian@invisible.ch")
-     (smtpmail-default-smtp-server "inv-home.dyndns.org")
-     (smtpmail-local-domain "invisible.ch")
-     (smtpmail-smtp-user "jens-christian")
-     (smtpmail-smtp-server "inv-home.dyndns.org")
-     (smtpmail-stream-type starttls)
-     (smtpmail-smtp-service 25)
-     )
-    ("zhaw"
-     (mu4e-sent-folder "/zhaw/INBOX.Sent Messages")
-     (mu4e-drafts-folder "/zhaw/INBOX.Drafts")
-     (mu4e-trash-folder "/zhaw/INBOX.Trash")
-     (user-mail-address "fisj@zhaw.ch")
-     (smtpmail-default-smtp-server "mail.zhaw.ch")
-     (smtpmail-local-domain "zhaw.ch")
-     (smtpmail-smtp-user "fisj@zhaw.ch")
-     (smtpmail-smtp-server "mail.zhaw.ch")
-     (smtpmail-stream-type starttls)
-     (smtpmail-smtp-service 456)
-     )
-    ("switch"
-     (mu4e-sent-folder "/dispatched/INBOX.Sent")
-     (mu4e-drafts-folder "/dispatched/INBOX.Drafts")
-     (mu4e-trash-folder "/dispatched/INBOX.Trash")
-     (user-mail-address "alain.lafon@dispatched.ch")
-     (smtpmail-default-smtp-server "mail.your-server.de")
-     (smtpmail-local-domain "dispatched.ch")
-     (smtpmail-smtp-user "munen@dispatched.ch")
-     (smtpmail-smtp-server "mail.your-server.de")
-     (smtpmail-stream-type starttls)
-     (smtpmail-smtp-service 25))))
-
-;; Whenever a new mail is to be composed, change all relevant
-;; configuration variables to the respective account. This method is
-;; taken from the MU4E documentation:
-;; http://www.djcbsoftware.nl/code/mu/mu4e/Multiple-accounts.html#Multiple-accounts
-;; (defun my-mu4e-set-account ()
-;;   "Set the account for composing a message."
-;;   (let* ((account
-;;           (if mu4e-compose-parent-message
-;;               (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-;;                 (string-match "/\\(.*?\\)/" maildir)
-;;                 (match-string 1 maildir))
-;;             (completing-read (format "Compose with account: (%s) "
-;;                                      (mapconcat #'(lambda (var) (car var))
-;;                                                 my-mu4e-account-alist "/"))
-;;                              (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-;;                              nil t nil nil (caar my-mu4e-account-alist))))
-;;          (account-vars (cdr (assoc account my-mu4e-account-alist))))
-;;     (if account-vars
-;;         (mapc #'(lambda (var)
-;;                   (set (car var) (cadr var)))
-;;               account-vars)
-;;       (error "No email account found"))))
 
 
-; (add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
 (add-hook 'mu4e-compose-mode-hook 'flyspell-mode)
 (add-hook 'mu4e-compose-mode-hook (lambda ()
                                    (ispell-change-dictionary "deutsch")))
@@ -191,82 +139,123 @@
 (add-hook 'message-send-hook 'my-message-check-attachment)
 
 
-;; html handling
-(setq mu4e-html2text-command
-  "textutil -stdin -format html -convert txt -stdout")
 
 ;; shortcuts
 
-;; (setq mu4e-maildir-shortcuts
-;;   '( ("/invisible/INBOX"     . ?i)
-;;      ("/invisible/archiv"   . ?a)
-;;      ("/lists"     . ?l)
-;;      ("/work"      . ?w)
-;;      ("/sent"      . ?s)))
+(setq mu4e-maildir-shortcuts
+  '( ("/invisible/INBOX"     . ?i)
+     ("/zhaw/INBOX"   . ?z)
+     ("/switch/INBOX"     . ?s)
+     ("/invisible/INBOX.Archiv"  . ?a)
+     ("/switch/INBOX.Archiv" . ?A)
+     ))
 
 ;; Contexts
 
- (setq mu4e-contexts
-    `( ,(make-mu4e-context
-	  :name "Invisible"
-	  :enter-func (lambda () (mu4e-message "Switch to the Invisible context"))
-	  ;; leave-func not defined
-	  :match-func (lambda (msg)
-			(when msg
-			  (mu4e-message-contact-field-matches msg
-			    :to "jens-christian@invisible.ch")))
-	  :vars '(  ( user-mail-address	     . "jens-christian@invisible.ch"  )
-              ( user-full-name	    . "Jens-Christian Fischer" )
-              ( mu4e-sent-folder . "/invisible/INBOX.Sent")
-              ( mu4e-drafts-folder . "/invisible/INBOX.Drafts" )
-              ( mu4e-trash-folder  . "/invisible/Trash" )
-                   (smtpmail-default-smtp-server . "inv-home.dyndns.org")
-                   (smtpmail-local-domain . "invisible.ch")
-                   (smtpmail-smtp-user . "jens-christian")
-                   (smtpmail-smtp-server . "inv-home.dyndns.org")
-                   (smtpmail-stream-type . starttls)
-                   (smtpmail-smtp-service . 25)
-		   ( mu4e-compose-signature .
-		     (concat
-		       "Jens-Christian Fischer\n"
-           "@jcfischer\n"
-           "+41 79 691 05 48\n"
-		       ))
-            ))
-       ,(make-mu4e-context
-	  :name "ZHAW"
-	  :enter-func (lambda () (mu4e-message "Switch to the ZHAW context"))
-	  ;; leave-fun not defined
-	  :match-func (lambda (msg)
-			(when msg
-			  (mu4e-message-contact-field-matches msg
-			    :to "fisj@zhaw.ch")))
-	  :vars '(  ( user-mail-address	     . "fisj@zhaw.ch" )
-		   ( user-full-name	    . "Jens-Christian Fischer" )
-           (mu4e-sent-folder . "/zhaw/INBOX.Sent Messages")
-     (mu4e-drafts-folder . "/zhaw/INBOX.Drafts")
-     (mu4e-trash-folder . "/zhaw/INBOX.Trash")
-     (user-mail-address . "fisj@zhaw.ch")
-     (smtpmail-default-smtp-server . "inv-home.dyndns.org")
-     (smtpmail-smtp-user . "jens-christian")
-     (smtpmail-smtp-server . "inv-home.dyndns.org")
-     (smtpmail-stream-type . starttls)
-     (smtpmail-smtp-service . 25)
+(setq mu4e-contexts
+      `( ,(make-mu4e-context
+           :name "Invisible"
+           :enter-func (lambda () (mu4e-message "Switch to the Invisible context"))
+           ;; leave-func not defined
+           :match-func (lambda (msg)
+                         (when msg
+                           (mu4e-message-contact-field-matches msg
+                                                               :to "jens-christian@invisible.ch")))
+           :vars '(  ( user-mail-address	     . "jens-christian@invisible.ch"  )
+                     ( user-full-name	    . "Jens-Christian Fischer" )
+                     ( mu4e-sent-folder . "/invisible/INBOX.Sent")
+                     ( mu4e-drafts-folder . "/invisible/INBOX.Drafts" )
+                     ( mu4e-trash-folder  . "/invisible/Trash" )
+                     (smtpmail-default-smtp-server . "inv-home.dyndns.org")
+                     (smtpmail-local-domain . "invisible.ch")
+                     (smtpmail-smtp-user . "jens-christian")
+                     (smtpmail-smtp-server . "inv-home.dyndns.org")
+                     (smtpmail-stream-type . starttls)
+                     (smtpmail-smtp-service . 25)
+                     ( mu4e-compose-signature .
+                                              (concat
+                                               "Jens-Christian Fischer\n"
+                                               "@jcfischer\n"
+                                               "+41 79 691 05 48\n"
+                                               ))
+                     )
+           )
+         ,(make-mu4e-context
+           :name "SWITCH"
+           :enter-func (lambda () (mu4e-message "Switch to the SWITCH context"))
+           ;; leave-func not defined
+           :match-func (lambda (msg)
+                         (when msg
+                           (mu4e-message-contact-field-matches msg
+                                                               :to "jens-christian.fischer@switch.ch")))
+           :vars '(  ( user-mail-address	     . "jens-christian.fischer@switch.ch"  )
+                     ( user-full-name	    . "Jens-Christian Fischer" )
+                     ( mu4e-sent-folder . "/switch/INBOX.Sent")
+                     ( mu4e-drafts-folder . "/switch/INBOX.Drafts" )
+                     ( mu4e-trash-folder  . "/switch/INBOX.Deleted Messages" )
+                     (smtpmail-default-smtp-server . "smtp.switch.ch")
+                     (smtpmail-local-domain . "switch.ch")
+                     (smtpmail-smtp-user . "jens-christian.fischer@switch.ch")
+                     (smtpmail-smtp-server . "smtp.switch.ch")
+                     (smtpmail-stream-type . starttls)
+                     (smtpmail-smtp-service . 25)
+                     ( mu4e-compose-signature .
+                                              (concat
+                                               "SWITCH\n"
+                                               "Jens-Christian Fischer, Peta Solutions\n"
+                                               "Werdstrasse 2, P.O. Box, 8021 Zurich, Switzerland\n"
+                                               "phone +41 44 268 15 15, direct +41 44 268 15 71\n"
+                                               "jens-christian.fischer@switch.ch\n"
+                                               "http://www.switch.ch\n"
+                                               "\n"
+                                               "http://www.switch.ch/stories\n"
+                                               ))
+                     )
+           )
 
-		   ( mu4e-compose-signature .
-		     (concat
-		       "Jens-Christian Fischer\n"
-		       "ZHAW IniT"))))))
+         ,(make-mu4e-context
+           :name "ZHAW"
+           :enter-func (lambda () (mu4e-message "Switch to the ZHAW context"))
+           ;; leave-fun not defined
+           :match-func (lambda (msg)
+                         (when msg
+                           (mu4e-message-contact-field-matches msg
+                                                               :to "fisj@zhaw.ch")))
+           :vars '(  ( user-mail-address	     . "fisj@zhaw.ch" )
+                     ( user-full-name	    . "Jens-Christian Fischer" )
+                     (mu4e-sent-folder . "/zhaw/INBOX.Sent Messages")
+                     (mu4e-drafts-folder . "/zhaw/INBOX.Drafts")
+                     (mu4e-trash-folder . "/zhaw/INBOX.Trash")
+                     (user-mail-address . "fisj@zhaw.ch")
+                     (smtpmail-default-smtp-server . "inv-home.dyndns.org")
+                     (smtpmail-local-domain . "invisible.ch")
+                     (smtpmail-smtp-user . "jens-christian")
+                     (smtpmail-smtp-server . "inv-home.dyndns.org")
+                     (smtpmail-stream-type . starttls)
+                     (smtpmail-smtp-service . 25)
+
+                     ( mu4e-compose-signature .
+                                              (concat
+                                               "Jens-Christian Fischer\n"
+                                               "ZHAW IniT"))))))
 
   ;; set `mu4e-context-policy` and `mu4e-compose-policy` to tweak when mu4e should
   ;; guess or ask the correct context, e.g.
 
   ;; start with the first (default) context;
   ;; default is to ask-if-none (ask when there's no context yet, and none match)
-  ;; (setq mu4e-context-policy 'pick-first)
+  (setq mu4e-context-policy 'pick-first)
 
   ;; compose with the current context is no context matches;
   ;; default is to ask
-  ;; '(setq mu4e-compose-context-policy nil)
+  (setq mu4e-compose-context-policy nil)
+
+
+;; fancy
+(setq mu4e-use-fancy-chars t)
+
+
+;; show images
+(setq mu4e-show-images t)
 
 ;;; mu4e-config.el ends here
